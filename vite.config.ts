7c9f3e8924cb45baae849cea35d68eab89929c1a
ADMin <pdrs.ajay.m@gmail.com>
2025-08-11 19:15:26 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+ 
+  build: {
+    rollupOptions: {
+      external: [], // don't externalize 'emailjs-com'
+    },
+  },
+
+
   base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
